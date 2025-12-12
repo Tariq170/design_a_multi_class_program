6ef48de924d571be1d40bdac1f1afd33d1173f27
Mohammed Tariq Ali <tariqali87b@gmail.com>
2025-12-12 16:53:30 +0000

    Update test_diary.py

--- tests/test_diary.py
+++ tests/test_diary.py
@@ -1,7 +1,7 @@
 from lib.diary import Diary
 from lib.diary_entry import DiaryEntry
 
-#Test 1: adds and retrieves entries
+'''#Test 1: adds and retrieves entries
 def test_add_plus_retrieve_entries():
     d = Diary()
     e = DiaryEntry("A", "Hello")
@@ -18,4 +18,13 @@ def test_filters_readable_entries():
     d.add(e1)
     d.add(e2)
 
-    assert d.entries_readable(2, 1) == [e1]
+    assert d.entries_readable(2, 1) == [e1]'''
+
+
+def test_diary_returns_phone_numbers():
+    d = Diary()
+    d.add(DiaryEntry("A", "text", "07123456789"))
+    d.add(DiaryEntry("B", "text2"))
+    d.add(DiaryEntry("C", "text3", "07987654321"))
+
+    assert d.all_phone_numbers() == ["07123456789", "07987654321"]
